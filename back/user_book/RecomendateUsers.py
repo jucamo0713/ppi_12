d13b29e2440211ba192a6bc554d2b0a10eddf8f7
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-11-26 22:18:32 -0500

    Update RecomendateUsers.py

--- back/user_book/RecomendateUsers.py
+++ back/user_book/RecomendateUsers.py
@@ -14,14 +14,31 @@ RECOMENDATE_USERS = APIRouter()
 @RECOMENDATE_USERS.get('/recomendate-users')
 def recomendate_users(request: Request,
                       authentication: str = Header(None)):
+    """
+    Endpoint para recomendar usuarios basados en libros leídos por el usuario autenticado.
+
+    Args:
+        request (Request): Objeto Request de FastAPI.
+        authentication (str): Token de autenticación pasado en la cabecera.
+
+    Returns:
+        dict: Diccionario que contiene la lista de usuarios recomendados.
+    """
+    # Validar el token de autenticación
     token_data = validate_token(authentication)
     user_id = token_data['id']
+
+    # Obtener usuarios seguidos por el usuario autenticado
     followings = search_following(request, authentication, None, None)
+
+    # Obtener datos consolidados de la base de datos
     data = list(request.app.database['user_books'].aggregate(
         consolidated_aggregate))[0]
     user_id = ObjectId(user_id)
+
     # Crear un grafo dirigido
     g = nx.DiGraph()
+
     # Agregar nodos y aristas al grafo
     for user in data["users"]:
         g.add_node(user["_id"], type="user", user=user)
@@ -33,21 +50,29 @@ def recomendate_users(request: Request,
                        read=relation["read"])
             g.add_edge(relation["book_id"], relation["user_id"],
                        read=relation["read"])
+
     # Libros que el usuario ha leído
     try:
-        succesors = g.successors(user_id)
+        successors = g.successors(user_id)
     except nx.exception.NetworkXError:
-        succesors = []
-    user_books = set(succesors)
+        successors = []
+    user_books = set(successors)
+
     other_users = set()
+
     for successor in user_books:
         # Obtener los sucesores de los sucesores del usuario
         other_users.update(g.successors(successor))
+
+    # Filtrar usuarios ya seguidos por el usuario
     other_users -= {user_id, *[follow.id for follow in followings["data"]]}
-    # Ordenar libros por la cantidad de usuarios que los han leído
+
+    # Ordenar usuarios por la cantidad de libros que han leído
     other_users = sorted(other_users, key=lambda x: g.in_degree(x),
                          reverse=True)
+
     other_users = other_users if len(other_users) > 0 else []
+
     return {
         "based_on_books_read": [User(**g.nodes[recommended]["user"]) for
                                 recommended in other_users[:5]],
